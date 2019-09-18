66163ca4c8f5e9b0d56aafa7b003ebf4bf792371
George Papadas <gpapadas@gmail.com>
2019-09-18 19:44:54 +0300

    Converting project to VS 2019 from VS 2010

--- ManageEmailLists/fMain.Designer.cs
+++ ManageEmailLists/fMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ManageEmailLists
 {
-    partial class Form1
+    partial class fMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnImportExcel = new System.Windows.Forms.Button();
+            this.btnExportEmailsWithoutDuplicates = new System.Windows.Forms.Button();
+            this.btnExportPrivateCorporateEmails = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnFindDuplicates = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImportExcel
+            // 
+            this.btnImportExcel.Location = new System.Drawing.Point(12, 22);
+            this.btnImportExcel.Name = "btnImportExcel";
+            this.btnImportExcel.Size = new System.Drawing.Size(93, 23);
+            this.btnImportExcel.TabIndex = 0;
+            this.btnImportExcel.Text = "Import...";
+            this.btnImportExcel.UseVisualStyleBackColor = true;
+            this.btnImportExcel.Click += new System.EventHandler(this.BtnImportExcel_Click);
+            // 
+            // btnExportEmailsWithoutDuplicates
+            // 
+            this.btnExportEmailsWithoutDuplicates.Location = new System.Drawing.Point(12, 80);
+            this.btnExportEmailsWithoutDuplicates.Name = "btnExportEmailsWithoutDuplicates";
+            this.btnExportEmailsWithoutDuplicates.Size = new System.Drawing.Size(93, 23);
+            this.btnExportEmailsWithoutDuplicates.TabIndex = 1;
+            this.btnExportEmailsWithoutDuplicates.Text = "button2";
+            this.btnExportEmailsWithoutDuplicates.UseVisualStyleBackColor = true;
+            this.btnExportEmailsWithoutDuplicates.Click += new System.EventHandler(this.BtnExportEmailsWithoutDuplicates_Click);
+            // 
+            // btnExportPrivateCorporateEmails
+            // 
+            this.btnExportPrivateCorporateEmails.Location = new System.Drawing.Point(12, 109);
+            this.btnExportPrivateCorporateEmails.Name = "btnExportPrivateCorporateEmails";
+            this.btnExportPrivateCorporateEmails.Size = new System.Drawing.Size(93, 23);
+            this.btnExportPrivateCorporateEmails.TabIndex = 2;
+            this.btnExportPrivateCorporateEmails.Text = "button3";
+            this.btnExportPrivateCorporateEmails.UseVisualStyleBackColor = true;
+            this.btnExportPrivateCorporateEmails.Click += new System.EventHandler(this.BtnExportPrivateCorporateEmails_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(126, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(197, 329);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnFindDuplicates
+            // 
+            this.btnFindDuplicates.Location = new System.Drawing.Point(12, 51);
+            this.btnFindDuplicates.Name = "btnFindDuplicates";
+            this.btnFindDuplicates.Size = new System.Drawing.Size(93, 23);
+            this.btnFindDuplicates.TabIndex = 4;
+            this.btnFindDuplicates.Text = "Find Duplicates";
+            this.btnFindDuplicates.UseVisualStyleBackColor = true;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(335, 375);
+            this.Controls.Add(this.btnFindDuplicates);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExportPrivateCorporateEmails);
+            this.Controls.Add(this.btnExportEmailsWithoutDuplicates);
+            this.Controls.Add(this.btnImportExcel);
+            this.Name = "fMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnImportExcel;
+        private System.Windows.Forms.Button btnExportEmailsWithoutDuplicates;
+        private System.Windows.Forms.Button btnExportPrivateCorporateEmails;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnFindDuplicates;
     }
 }
 
--- ManageEmailLists/fMain.cs
+++ ManageEmailLists/fMain.cs
@@ -6,15 +6,142 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+using System.Data.Common;
 using System.Windows.Forms;
 
 namespace ManageEmailLists
 {
-    public partial class Form1 : Form
+    public partial class fMain : Form
     {
-        public Form1()
+        private string connectionString;
+        private string filename, newFilename;
+        private List<string> initialList;
+        string[] sheetNames;
+
+        public fMain()
         {
             InitializeComponent();
+        }
+
+        private void BtnImportExcel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "ms excel 2003 files (*.xls)|*.xls|ms excel 2007 files (*.xlsx)|*.xlsx";
+            dialog.InitialDirectory = "C:";
+            dialog.Title = "Άνοιγμα αρχείου excel";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                filename = dialog.FileName;
+
+            initialList = new List<string>();
+
+            sheetNames = GetExcelSheetNames(filename);
+            connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;
+                Data Source=" + filename + ";Extended Properties=Excel 8.0;";
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.OleDb");
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                using (DbCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT Email FROM [" + sheetNames[0] + "]";
+
+                    connection.Open();
+
+                    using (DbDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            initialList.Add(reader["Email"].ToString());
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+
+            btnFindDuplicates.Enabled = true;
+            btnExportEmailsWithoutDuplicates.Enabled = true;
+            btnExportPrivateCorporateEmails.Enabled = true;
+        }
+
+        private void BtnExportEmailsWithoutDuplicates_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExportPrivateCorporateEmails_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private String[] GetExcelSheetNames(string excelFile)
+        {
+            OleDbConnection connection = null;
+            System.Data.DataTable dt = null;
+
+            try
+            {
+                // Create connection string.
+                //string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+                //    "Data Source=" + filename + ";Extended Properties=Excel 8.0;";
+                string connectionString =
+                    String.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=Excel 8.0;", excelFile);
+                //string connectionString =
+                //    String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 8.0;HDR=YES\";", excelFile);
+
+                // Create connection object by using the preceding connection string.
+                connection = new OleDbConnection(connectionString);
+                // Open connection with the database.
+                connection.Open();
+
+                // Get the data table containg the schema guid.
+                dt = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                if (dt == null)
+                {
+                    return null;
+                }
+
+                String[] excelSheets = new String[dt.Rows.Count];
+                int i = 0;
+
+                // Add the sheet name to the string array.
+                foreach (DataRow row in dt.Rows)
+                {
+                    excelSheets[i] = row["TABLE_NAME"].ToString();
+                    i++;
+                }
+
+                // Loop through all of the sheets if you want too...
+                for (int j = 0; j < excelSheets.Length; j++)
+                {
+                    // Query each excel sheet.
+                }
+
+                return excelSheets;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Exception: ", e);
+            }
+            finally
+            {
+                // Clean up.
+                if (connection != null)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+                if (dt != null)
+                {
+                    dt.Dispose();
+                }
+            }
         }
     }
 }
