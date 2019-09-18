060fa943818e026b15a13af54e3dfb890625663c
George Papadas <gpapadas@gmail.com>
2019-09-18 20:38:55 +0300

    Made find duplicates to work

--- ManageEmailLists/fMain.Designer.cs
+++ ManageEmailLists/fMain.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.btnImportExcel = new System.Windows.Forms.Button();
             this.btnExportEmailsWithoutDuplicates = new System.Windows.Forms.Button();
-            this.btnExportPrivateCorporateEmails = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnExportPrivateBusinessEmails = new System.Windows.Forms.Button();
+            this.lbDuplicateEmails = new System.Windows.Forms.ListBox();
             this.btnFindDuplicates = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -39,7 +39,7 @@
             // 
             this.btnImportExcel.Location = new System.Drawing.Point(12, 22);
             this.btnImportExcel.Name = "btnImportExcel";
-            this.btnImportExcel.Size = new System.Drawing.Size(93, 23);
+            this.btnImportExcel.Size = new System.Drawing.Size(163, 23);
             this.btnImportExcel.TabIndex = 0;
             this.btnImportExcel.Text = "Import...";
             this.btnImportExcel.UseVisualStyleBackColor = true;
@@ -47,49 +47,53 @@
             // 
             // btnExportEmailsWithoutDuplicates
             // 
+            this.btnExportEmailsWithoutDuplicates.Enabled = false;
             this.btnExportEmailsWithoutDuplicates.Location = new System.Drawing.Point(12, 80);
             this.btnExportEmailsWithoutDuplicates.Name = "btnExportEmailsWithoutDuplicates";
-            this.btnExportEmailsWithoutDuplicates.Size = new System.Drawing.Size(93, 23);
+            this.btnExportEmailsWithoutDuplicates.Size = new System.Drawing.Size(163, 23);
             this.btnExportEmailsWithoutDuplicates.TabIndex = 1;
-            this.btnExportEmailsWithoutDuplicates.Text = "button2";
+            this.btnExportEmailsWithoutDuplicates.Text = "Export emails with duplicates";
             this.btnExportEmailsWithoutDuplicates.UseVisualStyleBackColor = true;
             this.btnExportEmailsWithoutDuplicates.Click += new System.EventHandler(this.BtnExportEmailsWithoutDuplicates_Click);
             // 
-            // btnExportPrivateCorporateEmails
+            // btnExportPrivateBusinessEmails
             // 
-            this.btnExportPrivateCorporateEmails.Location = new System.Drawing.Point(12, 109);
-            this.btnExportPrivateCorporateEmails.Name = "btnExportPrivateCorporateEmails";
-            this.btnExportPrivateCorporateEmails.Size = new System.Drawing.Size(93, 23);
-            this.btnExportPrivateCorporateEmails.TabIndex = 2;
-            this.btnExportPrivateCorporateEmails.Text = "button3";
-            this.btnExportPrivateCorporateEmails.UseVisualStyleBackColor = true;
-            this.btnExportPrivateCorporateEmails.Click += new System.EventHandler(this.BtnExportPrivateCorporateEmails_Click);
+            this.btnExportPrivateBusinessEmails.Enabled = false;
+            this.btnExportPrivateBusinessEmails.Location = new System.Drawing.Point(12, 109);
+            this.btnExportPrivateBusinessEmails.Name = "btnExportPrivateBusinessEmails";
+            this.btnExportPrivateBusinessEmails.Size = new System.Drawing.Size(163, 23);
+            this.btnExportPrivateBusinessEmails.TabIndex = 2;
+            this.btnExportPrivateBusinessEmails.Text = "Export private and business emails";
+            this.btnExportPrivateBusinessEmails.UseVisualStyleBackColor = true;
+            this.btnExportPrivateBusinessEmails.Click += new System.EventHandler(this.BtnExportPrivateBusinessEmails_Click);
             // 
-            // listBox1
+            // lbDuplicateEmails
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(126, 22);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(197, 329);
-            this.listBox1.TabIndex = 3;
+            this.lbDuplicateEmails.FormattingEnabled = true;
+            this.lbDuplicateEmails.Location = new System.Drawing.Point(190, 22);
+            this.lbDuplicateEmails.Name = "lbDuplicateEmails";
+            this.lbDuplicateEmails.Size = new System.Drawing.Size(243, 329);
+            this.lbDuplicateEmails.TabIndex = 3;
             // 
             // btnFindDuplicates
             // 
+            this.btnFindDuplicates.Enabled = false;
             this.btnFindDuplicates.Location = new System.Drawing.Point(12, 51);
             this.btnFindDuplicates.Name = "btnFindDuplicates";
-            this.btnFindDuplicates.Size = new System.Drawing.Size(93, 23);
+            this.btnFindDuplicates.Size = new System.Drawing.Size(163, 23);
             this.btnFindDuplicates.TabIndex = 4;
-            this.btnFindDuplicates.Text = "Find Duplicates";
+            this.btnFindDuplicates.Text = "Find duplicates";
             this.btnFindDuplicates.UseVisualStyleBackColor = true;
+            this.btnFindDuplicates.Click += new System.EventHandler(this.BtnFindDuplicates_Click);
             // 
             // fMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 375);
+            this.ClientSize = new System.Drawing.Size(454, 375);
             this.Controls.Add(this.btnFindDuplicates);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.btnExportPrivateCorporateEmails);
+            this.Controls.Add(this.lbDuplicateEmails);
+            this.Controls.Add(this.btnExportPrivateBusinessEmails);
             this.Controls.Add(this.btnExportEmailsWithoutDuplicates);
             this.Controls.Add(this.btnImportExcel);
             this.Name = "fMain";
@@ -102,8 +106,8 @@
 
         private System.Windows.Forms.Button btnImportExcel;
         private System.Windows.Forms.Button btnExportEmailsWithoutDuplicates;
-        private System.Windows.Forms.Button btnExportPrivateCorporateEmails;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnExportPrivateBusinessEmails;
+        private System.Windows.Forms.ListBox lbDuplicateEmails;
         private System.Windows.Forms.Button btnFindDuplicates;
     }
 }
--- ManageEmailLists/fMain.cs
+++ ManageEmailLists/fMain.cs
@@ -19,6 +19,7 @@ namespace ManageEmailLists
         private string filename, newFilename;
         private string path;
         private List<string> initialList;
+        private List<string> emails;
         string[] sheetNames;
 
         public fMain()
@@ -28,6 +29,7 @@ namespace ManageEmailLists
 
         private void BtnImportExcel_Click(object sender, EventArgs e)
         {
+            // Open the file.
             OpenFileDialog dialog = new OpenFileDialog();
 
             dialog.Filter = "Ms excel 2003 files (*.xls)|*.xls|Ms excel 2007 files (*.xlsx)|*.xlsx";
@@ -37,6 +39,7 @@ namespace ManageEmailLists
             if (dialog.ShowDialog() == DialogResult.OK)
                 path = dialog.FileName;
 
+            // Read excel file.
             Excel.Application excelApp = new Excel.Application();
 
             if (excelApp != null)
@@ -46,8 +49,12 @@ namespace ManageEmailLists
                 Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelWorkbook.Sheets[1];
                 Excel.Range excelRange = excelWorksheet.UsedRange;
 
+                // Get rows and columns count.
                 int rows = excelRange.Rows.Count;
                 int cols = excelRange.Columns.Count;
+
+                // Initialize the emails list.
+                emails = new List<string>();
 
                 for (int i = 1; i <= rows; i++)
                 {
@@ -56,7 +63,8 @@ namespace ManageEmailLists
                         Excel.Range range = (excelWorksheet.Cells[i, 1] as Excel.Range);
                         string cellValue = range.Value.ToString();
 
-                        //TODO:
+                        // Add to list
+                        emails.Add(cellValue);
                     }
                 }
 
@@ -64,40 +72,21 @@ namespace ManageEmailLists
                 excelApp.Quit();
             }
 
-
-            //initialList = new List<string>();
-
-            //sheetNames = GetExcelSheetNames(filename);
-
-            //string connectionString =
-            //        String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 12.0;", filename);
-
-            //DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.OleDb");
-
-            //using (DbConnection connection = factory.CreateConnection())
-            //{
-            //    connection.ConnectionString = connectionString;
-
-            //    using (DbCommand command = connection.CreateCommand())
-            //    {
-            //        command.CommandText = "SELECT Email FROM [" + sheetNames[0] + "]";
-
-            //        connection.Open();
-
-            //        using (DbDataReader reader = command.ExecuteReader())
-            //        {
-            //            while (reader.Read())
-            //            {
-            //                initialList.Add(reader["Email"].ToString());
-            //            }
-            //        }
-            //        connection.Close();
-            //    }
-            //}
-
+            // Enable the other buttons.
             btnFindDuplicates.Enabled = true;
             btnExportEmailsWithoutDuplicates.Enabled = true;
-            btnExportPrivateCorporateEmails.Enabled = true;
+            btnExportPrivateBusinessEmails.Enabled = true;
+        }
+
+        private void BtnFindDuplicates_Click(object sender, EventArgs e)
+        {
+            List<string> duplicates = findDuplicates(emails);
+
+            lbDuplicateEmails.Items.Clear();
+            foreach (string value in duplicates)
+            {
+                lbDuplicateEmails.Items.Add(value);
+            }
         }
 
         private void BtnExportEmailsWithoutDuplicates_Click(object sender, EventArgs e)
@@ -105,27 +94,22 @@ namespace ManageEmailLists
 
         }
 
-        private void BtnExportPrivateCorporateEmails_Click(object sender, EventArgs e)
+        private void BtnExportPrivateBusinessEmails_Click(object sender, EventArgs e)
         {
 
         }
 
         private String[] GetExcelSheetNames(string excelFile)
         {
+            //TODO: Delete method.
             OleDbConnection connection = null;
             System.Data.DataTable dt = null;
 
             try
             {
                 // Create connection string.
-                //string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
-                //    "Data Source=" + filename + ";Extended Properties=Excel 8.0;";
-
                 string connectionString =
                     String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 12.0;", excelFile);
-
-                //string connectionString =
-                //    String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 8.0;HDR=YES\";", excelFile);
 
                 // Create connection object by using the preceding connection string.
                 connection = new OleDbConnection(connectionString);
@@ -175,6 +159,26 @@ namespace ManageEmailLists
                     dt.Dispose();
                 }
             }
+        }
+
+        private static List<string> findDuplicates(List<string> inputList)
+        {
+            List<string> duplicates = new List<string>();
+            HashSet<string> uniques = new HashSet<string>();
+
+            foreach (var input in inputList)
+            {
+                if (uniques.Contains(input))
+                {
+                    duplicates.Add(input);
+                }
+                else
+                {
+                    uniques.Add(input);
+                }
+            }
+
+            return duplicates.Distinct().ToList();
         }
     }
 }
